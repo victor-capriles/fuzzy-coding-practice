564edbf08844db9060d0ec4c66d2988d5c271abf
Victor Capriles <vdcapriles@gmail.com>
2025-09-05 10:58:53 +0200

    finished vmachine

--- OOP/E5_vendingMachine.js
+++ OOP/E5_vendingMachine.js
@@ -29,43 +29,89 @@ Apply OOP concepts:
 */
 
 class Product {
-    constructor (name, price, quantity) {
+    constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
     // method to purchase, decrease quantity
-    purchase () {
+    purchase() {
         // fail fast
         if (this.quantity <= 0) {
             return ("Not enough stock");
         }
-            this.quantity -= 1;
-            return (`Thanks for your purchase. Current stock ${this.quantity}`)
+        this.quantity -= 1;
+        return (`Thanks for your purchase. Current stock ${this.quantity}`)
     }
 }
 
 class Snack extends Product {
-    constructor (name, price, quantity, calories) {
+    constructor(name, price, quantity, calories) {
         super(name, price, quantity);
         this.calories = calories;
     }
 
     // override the purchase method to track calories
-    purchase () {
+    purchase() {
         if (this.quantity <= 0) {
             return ("Not enough stock");
         }
 
         this.quantity -= 1;
-        return (`Thanks for buying the snack ${this.name}, it contains ${this.calories}. Current stock ${this.stock}`);
+        return (`Thanks for buying the snack ${this.name}, it contains ${this.calories}. Current stock ${this.quantity}`);
     }
 }
 
 class VendingMachine {
-    constructor(balance) {
+    // make balance private and expose a getter
+    #balance;
+
+    constructor(balance = 0) {
         this.products = [];
-        this.balance = balance;
+        this.#balance = balance;
+    }
+
+    getBalance() {
+        return this.#balance;
+    }
+
+    insertCoin(amount) {
+        if (amount <= 0) {
+            return ("Invalid amount");
+        }
+        this.#balance += amount;
+
+        return (`Inserted ${amount}. Current machine balance ${this.#balance}`);
+    }
+
+    addProduct (product) {
+        this.products.push(product);
+    }
+
+    selectProduct(productName) {
+        const product = this.products.find(p => p.name === productName);
+
+        if (!product) {
+            return ("Product not found");
+        }
+        if (product.quantity <= 0) {
+            return ("Not enough stock");
+        }
+        if (this.#balance < product.price) {
+            return ("not enough balance");
+        }
+
+        // call method purchase
+        product.purchase();
+        this.#balance -= product.price;
+
+        return (`Dispensed ${product.name}. Remaining balance ${this.#balance}`);
+    }
+
+    refund() {
+        const refundAmount = this.#balance;
+        this.#balance = 0;
+        return `Refunded: $${refundAmount}`;
     }
 }
