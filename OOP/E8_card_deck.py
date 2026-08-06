e8d052a25fa778ef760c37c01df9365124f0b742
Victor Capriles <87203183+victor-capriles@users.noreply.github.com>
2026-08-06 17:27:28 +0200

    added new emulating numeric types examples

--- OOP/E8_card_deck.py
+++ OOP/E8_card_deck.py
@@ -4,13 +4,16 @@ import collections
 # namedtuple to build classes of objects
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
+# card ranks
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
 # class to define what a deck is
 class FrenchDeck:
     # create al possible card ranks; convert numbers to strings 
     # and then list JQKA
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     # take this string and split it wherever there's a whitespace
-    suits = 'space diamonds clubs hearts'.split()
+    suits = 'spades diamonds clubs hearts'.split()
 
     # when a new french deck is created, create a list of
     # all cards and store it inside this deck
@@ -32,6 +35,16 @@ class FrenchDeck:
         # return the card at the requested position in the deck
         return self._cards[position]
 
+    
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+    
+    
 deck = FrenchDeck()
 print(len(deck))
 print(deck[0])
+
+for card in sorted(deck, key=FrenchDeck.spades_high):
+    print(card)
--- OOP/E9_book.py
+++ OOP/E9_book.py
@@ -1,4 +1,5 @@
 import collections
+from random import choice
 
 Book = collections.namedtuple("Book", ["title", "author"])
 book = Book("Coding in Pain", "Victor")
@@ -7,10 +8,11 @@ print(book.title)
 print(book.author)
 
 class Library:
-    def __innit__(self):
+    def __init__(self):
         self._books = [
             Book("fluent in python", "Luciano R"),
-            Book("refactoring", "Martin F")
+            Book("refactoring", "Martin F"),
+            Book("harry potter", "J.K")
         ]
 
     def __len__(self):
@@ -18,3 +20,17 @@ class Library:
 
     def __getitem__(self, position):
         return self._books[position]
+
+library = Library()
+print(len(library))
+print(library[0])
+print(library[1])
+print(choice(library))
+
+for bookie in library:
+    print(bookie)
+
+for bookies in reversed(library):
+    print("Reversed order", bookies)
+
+print(Book("fluent in python", "Luciano R") in library)
